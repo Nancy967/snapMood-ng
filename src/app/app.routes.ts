278ff348c752836e093328c0a20be0138c95ef6f
shixun2024 <104483886+Nancy967@users.noreply.github.com>
2024-06-25 22:39:10 +0800

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { AboutsComponent } from './components/abouts/abouts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PostsComponent },
+  { path: 'abouts', component: AboutsComponent },
+];
