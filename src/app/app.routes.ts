db8f067995669eacb41d9d89017835e7bf423085
shixun2024 <104483886+Nancy967@users.noreply.github.com>
2024-06-28 23:01:53 +0800

    add sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './components/posts/posts.component';
 import { AboutsComponent } from './components/abouts/abouts.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { VideosComponent } from './components/videos/videos.component';
 
 export const routes: Routes = [
-  { path: '', component: PostsComponent },
+  { path: 'posts', component: PostsComponent },
   { path: 'abouts', component: AboutsComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
 ];
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,19 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgFor],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-    title = "sidebar works!"
+  menuItems = [
+    { label: 'Posts', link: '/posts' },
+    { label: 'Abouts', link: '/abouts' },
+    { label: 'Photos', link: '/photos' },
+    { label: 'Videos', link: '/videos' },
+  ];
 }
